a7bb4ee5c85ca34764c4566201722c08f6fbfb9f
AntonSynytsia <anton.synytsia@gmail.com>
2020-03-27 17:06:01 -0700

    Checking ComposableMap and Geographies example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/Main";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Main />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+
+import "./MapChart";
+import MapChart from "./MapChart";
+
+const Main = props => {
+  return (
+    <Fragment>
+      <MapChart />
+    </Fragment>
+  );
+};
+
+Main.propTypes = {};
+
+export default Main;
